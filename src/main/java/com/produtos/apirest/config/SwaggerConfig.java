7c504ccfe64cd9e050fe60316e8fd066bdcd913d
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2022-01-16 21:44:32 -0300

    Primeira API REST com Spring Boot e MySQL

---
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.produtos.apirest.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Component
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.produtos.apirest"))
+                .paths(PathSelectors.regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Produtos API REST",
+                "API REST de cadastro de produtos.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Daniel Pinheiro", "https://www.linkedin.com/in/dnldev/",
+                        "daniel.ramos5291@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+
+}
+	
